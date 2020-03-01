d9b664a1c77a2ca875365aa6d3e932b166468a09
anguyen120 <anguyen120@uh.edu>
2020-02-29 22:57:31 -0600

    Connected Java backend with "GUI", Players' form can be null, teams and hometown fields can be autocomplete through jQuery, players_form.js implemented to handle form submission and autocomplete items, links to players_list.html after submission

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,17 +1,30 @@
 package application;
 
 import javafx.application.Application;
+import javafx.concurrent.Worker;
 import javafx.geometry.Rectangle2D;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.layout.StackPane;
+import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebView;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
+import netscape.javascript.JSObject;
 
 import java.io.File;
 
 public class Main extends Application {
+  /**
+   * for communication to the Javascript engine.
+   */
+  private JSObject javascriptConnector;
+
+  /**
+   * for communication from the Javascript engine.
+   */
+  private JavaConnector javaConnector = new JavaConnector();
+
   public static void main(String[] args) {
     launch(args);
   }
@@ -21,7 +34,17 @@ public class Main extends Application {
 
     WebView webView = new WebView();
     File html = new File("html/index.html");
-    webView.getEngine().load(html.toURI().toString());
+    webView.setContextMenuEnabled(false);
+    WebEngine webEngine = webView.getEngine();
+    webEngine.load(html.toURI().toString());
+
+    webEngine.getLoadWorker().stateProperty().addListener((observable, oldValue, newValue) -> {
+      if (Worker.State.SUCCEEDED == newValue) {
+        // set an interface object named 'javaConnector' in the web engine's page
+        JSObject window = (JSObject) webEngine.executeScript("window");
+        window.setMember("javaConnector", javaConnector);
+      }
+    });
 
     root.getChildren().add(webView);
 
@@ -29,11 +52,18 @@ public class Main extends Application {
     Scene scene = new Scene(root, visualBounds.getWidth(), visualBounds.getHeight());
 
     File logo = new File("html/images/logo.png");
-
     primaryStage.getIcons().add(new Image(logo.toURI().toString()));
     primaryStage.setTitle("Sportiza");
     primaryStage.setScene(scene);
     primaryStage.setMaximized(true);
     primaryStage.show();
+  }
+
+  public class JavaConnector {
+    /**
+     * called when the JS side wants a query to database
+     *
+     */
+
   }
 }
