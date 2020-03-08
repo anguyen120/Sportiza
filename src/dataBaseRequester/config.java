92e94da94e90f5654e3d98ea6ebb3942b0937bdb
Edgar Portales <edgar_ivanhdz15@hotmail.com>
2020-03-07 21:54:08 -0600

    Player Stats Request

--- src/dataBaseRequester/config.java
+++ src/dataBaseRequester/config.java
@@ -1,10 +1,8 @@
 
 public class config {
-	public static String username = "edgariphdz98";
-	public static String credential = "426005331";
-	public static String databaseName = "jdbc:postgresql://csce-315-db.engr.tamu.edu/sportizadevspace";
-	public static String requestPlayerFormFile = "requestFiles/playerFormRequest.json";
-	public static String playerStatsRequestFile = "requestFiles/playerStatsRequest.json";
-	
-	
+    public static String username = "edgariphdz98";
+    public static String credential = "426005331";
+    public static String databaseName = "jdbc:postgresql://csce-315-db.engr.tamu.edu/sportizadevspace";
+    public static String requestPlayerFormFile = "requestFiles/playerFormRequest.json";
+    public static String playerStatsRequestFile = "requestFiles/playerStatsRequest.json";
 }
--- src/dataBaseRequester/main.java
+++ src/dataBaseRequester/main.java
@@ -1,17 +1,17 @@
 public class main {
-	////////////////////////////////////////
-	//MAKE SURE the directory "requestFiles" is created before requesting anything
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub  
-		//constructor to set up the connector between database and this class
-		connectorPostgres connector = new connectorPostgres(config.username,config.credential,config.databaseName);
-		
-		// requesting form with parameters in following order
-		//FirstName LastName TeamName UniformNumber HomeTown
-		//connector.playerFormRequest("NULL", "NULL", "UCLA", "NULL", "Plano");
-		connector.playerFormRequest("NULL", "NULL", "Texas A&M", "NULL", "NULL");
-		//connector.playerStatsRequest("1041700");
-	}
-	
+    ////////////////////////////////////////
+    //MAKE SURE the directory "requestFiles" is created before requesting anything
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        //constructor to set up the connector between database and this class
+        connectorPostgres connector = new connectorPostgres(config.username,config.credential,config.databaseName);
+
+        // requesting form with parameters in following order
+        //FirstName LastName TeamName UniformNumber HomeTown
+        //connector.playerFormRequest("NULL", "NULL", "UCLA", "NULL", "Plano");
+        //connector.playerFormRequest("NULL", "NULL", "Texas A&M", "NULL", "NULL");
+        connector.playerStatsRequest("1041700");
+    }
+
 
 }
